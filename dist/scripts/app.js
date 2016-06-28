98fd344d6e7b131e19863dd5832df8e8cb0d8504
Gabriel Gonzaga <gabrielmgonzaga@gmail.com>
2016-06-27 23:00:40 -0700

    commit 2

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,28 @@
-angular.module('blocJams', []);
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true, // hashbang urls disabled
+                requireBase: false // false to avoid $location error
+        });
+        
+        $stateProvider // .state (stateName, stateConfig)
+            .state('landing', {
+                url: '/',
+                controller: 'LandingCtrl as landing',
+                templateUrl: '/templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            })
+            .state('collection', {
+                url: '/collection',
+                templateUrl: '/templates/collection.html'
+            });
+    };
+    
+    angular 
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();
